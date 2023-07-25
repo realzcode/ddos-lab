1cdc637d36a5c8e39accaba1ae8084a094101d6c
Realz Code <114070103+realzcode@users.noreply.github.com>
2023-07-25 15:08:02 +0700

    DDOS-LAB

--- index.php
+++ index.php
@@ -14,168 +14,168 @@ header("Accept: text/html; charset=UTF-8");
 header("Set-Cookie: realzcode=ddos-lab-v1.0-$date; Path=/");
 
 $browser_user_agent = $_SERVER['HTTP_USER_AGENT'];
+$REQUEST_METHOD = $_SERVER['REQUEST_METHOD'];
 
 if (isset($_GET['reset']))
 {
-	$x = "./get.txt";
-	$y = fopen($x, "w+");
-	fwrite($y, 0);
-	fclose($y);
+   $x = "./get.txt";
+   $y = fopen($x, "w+");
+   fwrite($y, 0);
+   fclose($y);
 
-	$x = "./post.txt";
-	$y = fopen($x, "w+");
-	fwrite($y, 0);
-	fclose($y);
+   $x = "./post.txt";
+   $y = fopen($x, "w+");
+   fwrite($y, 0);
+   fclose($y);
 
-	$x = "./head.txt";
-	$y = fopen($x, "w+");
-	fwrite($y, 0);
-	fclose($y);
+   $x = "./head.txt";
+   $y = fopen($x, "w+");
+   fwrite($y, 0);
+   fclose($y);
 
-	$x = "./others.txt";
-	$y = fopen($x, "w+");
-	fwrite($y, 0);
-	fclose($y);
+   $x = "./others.txt";
+   $y = fopen($x, "w+");
+   fwrite($y, 0);
+   fclose($y);
 
-	header("location: .");
+   header("location: .");
 }
 
-if ($browser_user_agent !== $_SERVER['HTTP_USER_AGENT']) 
+if ($browser_user_agent !== $browser_user_agent) 
 {
-	$ip_address = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
+   $ip_address = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
 
-	echo "REALZCODE DDOS-LAB V1.0\r\n";
-	echo "Method: " . $_SERVER['REQUEST_METHOD'] . "\r\n";
-	echo "User-Agent: " . $_SERVER['HTTP_USER_AGENT'] . "\r\n";
-	echo "Ip-Address: $ip_address\r\n";
+   echo "REALZCODE DDOS-LAB V1.0\r\n";
+   echo "Method: $REQUEST_METHOD\r\n";
+   echo "User-Agent: $browser_user_agent\r\n";
+   echo "Ip-Address: $ip_address\r\n";
 
-	if ($_SERVER['REQUEST_METHOD'] === 'GET') 
-	{
-		$x = "./get.txt";
-		$y = fopen($x, "r");
-		$z = 0;
-		if (filesize($x) > 0) 
-		{
-			$z = fread($y, filesize($x));
-		}
-		$count = $z + 1;
-		fclose($y);
-		$y = fopen($x, "w+");
-		fwrite($y, $count);
-		fclose($y);
-	} 
-	elseif ($_SERVER['REQUEST_METHOD'] === 'POST') 
-	{
-		$x = "./post.txt";
-		$y = fopen($x, "r");
-		$z = 0;
-		if (filesize($x) > 0) 
-		{
-			$z = fread($y, filesize($x));
-		}
-		$count = $z + 1;
-		fclose($y);
-		$y = fopen($x, "w+");
-		fwrite($y, $count);
-		fclose($y);
-	} 
-	elseif ($_SERVER['REQUEST_METHOD'] === 'HEAD') 
-	{
-		$x = "./head.txt";
-		$y = fopen($x, "r");
-		$z = 0;
-		if (filesize($x) > 0) 
-		{
-			$z = fread($y, filesize($x));
-		}
-		$count = $z + 1;
-		fclose($y);
-		$y = fopen($x, "w+");
-		fwrite($y, $count);
-		fclose($y);
-	} 
-	else
-	{
-		$x = "./others.txt";
-		$y = fopen($x, "r");
-		$z = 0;
-		if (filesize($x) > 0) 
-		{
-			$z = fread($y, filesize($x));
-		}
-		$count = $z + 1;
-		fclose($y);
-		$y = fopen($x, "w+");
-		fwrite($y, $count);
-		fclose($y);
-	}
+   if ($REQUEST_METHOD === 'GET') 
+   {
+      $x = "./get.txt";
+      $y = fopen($x, "r");
+      $z = 0;
+      if (filesize($x) > 0) {
+         $z = fread($y, filesize($x));
+      }
+      $count = $z + 1;
+      fclose($y);
+      $y = fopen($x, "w+");
+      fwrite($y, $count);
+      fclose($y);
+   } 
+   elseif ($REQUEST_METHOD === 'POST') 
+   {
+      $x = "./post.txt";
+      $y = fopen($x, "r");
+      $z = 0;
+      if (filesize($x) > 0) {
+         $z = fread($y, filesize($x));
+      }
+      $count = $z + 1;
+      fclose($y);
+      $y = fopen($x, "w+");
+      fwrite($y, $count);
+      fclose($y);
+   } 
+   elseif ($REQUEST_METHOD === 'HEAD') 
+   {
+      $x = "./head.txt";
+      $y = fopen($x, "r");
+      $z = 0;
+      if (filesize($x) > 0) {
+         $z = fread($y, filesize($x));
+      }
+      $count = $z + 1;
+      fclose($y);
+      $y = fopen($x, "w+");
+      fwrite($y, $count);
+      fclose($y);
+   } 
+   else
+   {
+      $x = "./others.txt";
+      $y = fopen($x, "r");
+      $z = 0;
+      if (filesize($x) > 0) {
+         $z = fread($y, filesize($x));
+      }
+      $count = $z + 1;
+      fclose($y);
+      $y = fopen($x, "w+");
+      fwrite($y, $count);
+      fclose($y);
+   }
 }
 else
 {
-	$x = "./get.txt";
-	$y = fopen($x, "r");
-	$get = 0;
-	if (filesize($x) > 0) {
-	$get = fread($y, filesize($x));
-	}
-	fclose($y);
+   $x = "./get.txt";
+   $y = fopen($x, "r");
+   $get = 0;
+   if (filesize($x) > 0) {
+      $get = fread($y, filesize($x));
+   }
+   fclose($y);
 
-	$x = "./post.txt";
-	$y = fopen($x, "r");
-	$post = 0;
-	if (filesize($x) > 0) {
-	$post = fread($y, filesize($x));
-	}
-	fclose($y);
+   $x = "./post.txt";
+   $y = fopen($x, "r");
+   $post = 0;
+   if (filesize($x) > 0) {
+      $post = fread($y, filesize($x));
+   }
+   fclose($y);
 
-	$x = "./head.txt";
-	$y = fopen($x, "r");
-	$head = 0;
-	if (filesize($x) > 0) {
-	$head = fread($y, filesize($x));
-	}
-	fclose($y);
+   $x = "./head.txt";
+   $y = fopen($x, "r");
+   $head = 0;
+   if (filesize($x) > 0) {
+   $head = fread($y, filesize($x));
+   }
+   fclose($y);
 
-	$x = "./others.txt";
-	$y = fopen($x, "r");
-	$others = 0;
-	if (filesize($x) > 0) {
-	$others = fread($y, filesize($x));
-	}
-	fclose($y);
+   $x = "./others.txt";
+   $y = fopen($x, "r");
+   $others = 0;
+   if (filesize($x) > 0) {
+   $others = fread($y, filesize($x));
+   }
+   fclose($y);
 
 echo "<center style=font-family:consolas>
-<b>DDOS-LAB V1.0</b>
-<br>by RealzCode<br>
-<br><table border=1>
-<thead>
-<tr>
-<th width=77>METHOD</th>
-<th width=77>COUNT</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<td>GET</td>
-<td align=center>$get</td>
-</tr>
-<tr>
-<td>POST</td>
-<td align=center>$post</td>
-</tr>
-<tr>
-<td>HEAD</td>
-<td align=center>$head</td>
-</tr>
-<tr>
-<td>OTHERS</td>
-<td align=center>$others</td>
-</tr>
-</tbody>
-</table><br>
-<a href=?reset>Reset</a> | <a href=.>Refresh</a>
-</center>";
-
+   <b>DDOS-LAB V1.0</b>
+   <br>
+   by RealzCode
+   <br>
+   <br>
+    <table border=1>
+      <thead>
+         <tr>
+            <th width=77>METHOD</th>
+            <th width=77>COUNT</th>
+         </tr>
+      </thead>
+      <tbody>
+         <tr>
+            <td>GET</td>
+            <td align=center>$get</td>
+         </tr>
+         <tr>
+            <td>POST</td>
+            <td align=center>$post</td>
+         </tr>
+         <tr>
+            <td>HEAD</td>
+            <td align=center>$head</td>
+         </tr>
+         <tr>
+            <td>OTHERS</td>
+            <td align=center>$others</td>
+         </tr>
+      </tbody>
+   </table>
+   <br>
+   <a href=?reset>Reset</a> | <a href=.>Refresh</a>
+   </center>";
 }
 
 ?>
